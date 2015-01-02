bf39af0f5a01256254e768a17b4c1d243a7db9e0
ichigotake <ichigotake.san@gmail.com>
2015-01-02 13:21:59 +0900

    Implement TableCursor

---
+++ sqlitehelper/src/main/java/net/ichigotake/sqlitehelper/TableCursor.java
@@ -0,0 +1,42 @@
+package net.ichigotake.sqlitehelper;
+
+import android.database.Cursor;
+
+import net.ichigotake.sqlitehelper.ddl.TableField;
+
+public class TableCursor {
+
+    final private Cursor mCursor;
+
+    public TableCursor(Cursor c) {
+        mCursor = c;
+    }
+    
+    public boolean moveToFirst() {
+        return mCursor.moveToFirst();
+    }
+    
+    public boolean moveToNext() {
+        return mCursor.moveToNext();
+    }
+    
+    public byte[] getBlob(TableField field) {
+        return mCursor.getBlob(mCursor.getColumnIndex(field.getFieldName()));
+    }
+
+    public int getInt(TableField field) {
+        return mCursor.getInt(mCursor.getColumnIndex(field.getFieldName()));
+    }
+
+    public long getLong(TableField field) {
+        return mCursor.getLong(mCursor.getColumnIndex(field.getFieldName()));
+    }
+
+    public String getString(TableField field) {
+        return mCursor.getString(mCursor.getColumnIndex(field.getFieldName()));
+    }
+
+    public void close() {
+        mCursor.close();
+    }
+}
