70ea0be75d707c264747b32676dd92417146112d
Takayuki Otake <otake@studyplus.jp>
2015-05-12 17:07:58 +0900

    Update libraries

--- sqlitehelper/src/test/java/net/ichigotake/sqlitehelper/dml/SelectTest.java
+++ sqlitehelper/src/test/java/net/ichigotake/sqlitehelper/dml/SelectTest.java
@@ -1,25 +1,28 @@
 package net.ichigotake.sqlitehelper.dml;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import junit.framework.Assert;
 
+import net.ichigotake.sqlitehelper.BuildConfig;
 import net.ichigotake.sqlitehelper.DatabaseHelper;
 import net.ichigotake.sqlitehelper.MockConfiguration;
 import net.ichigotake.sqlitehelper.MockTableDefinition;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
 
-@Config(emulateSdk = 18)
-@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+@RunWith(RobolectricGradleTestRunner.class)
 public class SelectTest {
     
     private SQLiteDatabase database() {
-        return new DatabaseHelper(Robolectric.application, new MockConfiguration())
+        Context context = ShadowApplication.getInstance().getApplicationContext();
+        return new DatabaseHelper(context, new MockConfiguration())
                 .getReadableDatabase();
     }
 
