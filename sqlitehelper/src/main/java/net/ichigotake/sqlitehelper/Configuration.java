e18e922baf3b7797305a5eff434e80772a60790f
ichigotake <ichigotake.san@gmail.com>
2015-08-23 16:45:31 +0900

    Add NonNull annotation

--- sqlitehelper/src/main/java/net/ichigotake/sqlitehelper/Configuration.java
+++ sqlitehelper/src/main/java/net/ichigotake/sqlitehelper/Configuration.java
@@ -1,17 +1,24 @@
 package net.ichigotake.sqlitehelper;
 
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+
 import net.ichigotake.sqlitehelper.schema.TableDefinition;
 
 import java.util.List;
 
 public interface Configuration {
-    
+
+    @NonNull
     List<TableDefinition> getDatabaseTables();
-    
+
+    @IntRange(from = 0, to = Integer.MAX_VALUE)
     int getDatabaseVersion();
-    
+
+    @NonNull
     String getDatabaseName();
 
+    @NonNull
     MigrationCallback getMigrationCallback();
     
 }
--- sqlitehelper/src/test/java/net/ichigotake/sqlitehelper/MockConfiguration.java
+++ sqlitehelper/src/test/java/net/ichigotake/sqlitehelper/MockConfiguration.java
@@ -1,5 +1,7 @@
 package net.ichigotake.sqlitehelper;
 
+import android.support.annotation.NonNull;
+
 import net.ichigotake.sqlitehelper.schema.TableDefinition;
 
 import java.util.Arrays;
@@ -7,6 +9,7 @@ import java.util.List;
 
 public class MockConfiguration implements Configuration {
     
+    @NonNull
     @Override
     public List<TableDefinition> getDatabaseTables() {
         return Arrays.<TableDefinition>asList(
@@ -19,11 +22,13 @@ public class MockConfiguration implements Configuration {
         return 1;
     }
 
+    @NonNull
     @Override
     public String getDatabaseName() {
         return "mock.db";
     }
 
+    @NonNull
     @Override
     public MigrationCallback getMigrationCallback() {
         return new NoMigrationCallback();
